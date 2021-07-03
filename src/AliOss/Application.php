af2bccf689674898462ca0cb0e8c334312add7a4
guozi <33157@163.com>
2021-07-03 23:10:34 +0800

    聚合API更新  1.2.1

---
+++ src/AliOss/Application.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace JuheApi\AliOss;
+
+use JuheApi\Kernel\Response;
+use JuheApi\BasicService\BaseConfig;
+use JuheApi\BasicService\RequestContainer;
+
+/**
+ * Class Application.
+ */
+class Application extends RequestContainer
+{
+    use Response;
+    use BaseConfig;
+    
+    // 服务标识
+    private $serverMark = 'oss';
+    
+    public function __construct(array $config = [], array $prepends = [], string $id = null)
+    {
+        parent::__construct($this->initConfig($config, $this->serverMark), $this->serverMark, true);
+    }
+    
+    // 上传文件
+    public function upload($params = [])
+    {
+        if (!isset($params['path']) || !file_exists($params['path'])) {
+            $this->fail('图片路径有误，请检查。');
+        }
+        if (!isset($params['filename'])) {
+            $params['filename'] = basename($params['path']);
+        }
+        return $this->httpUploadV1(
+            [
+                'file' => $params['path'],
+            ],
+            array_merge([
+                'action' => 'upload',
+            ], $params)
+        );
+    }
+    
+    // 删除文件
+    public function delele($files)
+    {
+        $params = [
+            'files' => is_array($files) ? implode(',', $files) : $files,
+        ];
+        return $this->common('del', $params);
+    }
+    
+    // 复制文件
+    public function copy($params = [])
+    {
+        return $this->common('copy', $params);
+    }
+    
+    // 移动文件
+    public function move($params = [])
+    {
+        return $this->common('move', $params);
+    }
+    
+    // 取文件信息
+    public function getFileInfo($file)
+    {
+        return $this->common('get_file_info', [
+            'file' => $file,
+        ]);
+    }
+    
+    // 取文件信息
+    public function getImageInfo($file)
+    {
+        return $this->common('get_image_info', [
+            'file' => $file,
+        ]);
+    }
+    
+    // 生成视频图片
+    public function genVideoImage($file)
+    {
+        return $this->common('gen_video_image', [
+            'file' => $file,
+        ]);
+    }
+    
+    // 文件是否存在
+    public function fileExist($file)
+    {
+        return $this->common('file_exist', [
+            'file' => $file,
+        ]);
+    }
+    
+    // 取上传Token
+    public function getToken()
+    {
+        return $this->common('get_token', []);
+    }
+    
+    // 取文件列表
+    public function getFileList($params = [])
+    {
+        return $this->common('list', $params);
+    }
+    
+    private function common($action, $params = [])
+    {
+        return $this->httpPostJsonV1(
+            array_merge([
+                'action' => $action,
+            ], $params)
+        );
+    }
+}
--- src/MiniProgram/Message/ServiceProvider.php
+++ src/MiniProgram/Message/ServiceProvider.php
@@ -28,4 +28,75 @@ class ServiceProvider extends RequestContainer
     {
         return $this->httpPostJsonV2(array_merge(['action' => 'sendImage'], $params));
     }
+    
+    public function sendImageAutoUpload($params = [])
+    {
+        if (!isset($params['imagePath']) || !file_exists($params['imagePath'])) {
+            $this->fail('图片文件不存在，请检查');
+        }
+        
+        if ($this->getFileType(file_get_contents($params['imagePath'])) !== 0) {
+            $this->fail('当前只支持图片格式');
+        }
+        
+        if (!isset($params['touser'])) {
+            $this->fail('touser 不能为空');
+        }
+        
+        return $this->httpUploadV2(
+            [
+                'file' => $params['imagePath'],
+            ],
+            [
+                'action'   => 'sendImageAutoUpload',
+                'touser'   => $params['touser'],
+                'filename' => pathinfo($params['imagePath'], PATHINFO_BASENAME),
+            ]
+        );
+    }
+    
+    
+    private function getFileType($file, &$fileType = '')
+    {
+        // 文件头标识 (2 bytes)
+        $bin     = substr($file, 0, 2);
+        $strInfo = @unpack("C2chars", $bin);;
+        $typeCode = intval($strInfo['chars1'] . $strInfo['chars2']);
+        $fileType = "";
+        switch ($typeCode) {
+            case 255216 :
+                $fileType = "image/jpeg";
+                $ret      = 0;
+                break;
+            case 7173 :
+                $fileType = "image/gif";
+                $ret      = 0;
+                break;
+            case 13780 :
+                $fileType = "image/png";
+                $ret      = 0;
+                break;
+            case 6677:
+                $fileType = 'image/bmp';
+                $ret      = 0;
+                break;
+            case 7790:
+                $fileType = 'exe';
+                $ret      = 0;
+                break;
+            case 7784:
+                $fileType = 'midi';
+                $ret      = 0;
+                break;
+            case 8297:
+                $fileType = 'rar';
+                $ret      = 0;
+                break;
+            default :
+                $fileType = "unknow";
+                $ret      = -1;
+                break;
+        }
+        return $ret;
+    }
 }
