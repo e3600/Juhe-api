783f9a28f949dc8f52312e4d5d62994c6fcbbdff
guozi <33157@163.com>
2021-10-16 17:43:39 +0800

    修复短信，公众号验证码类

--- src/Vcode/Application.php
+++ src/Vcode/Application.php
@@ -11,33 +11,50 @@ use JuheApi\BasicService\RequestContainer;
  */
 class Application extends RequestContainer
 {
-    use Response;
-    use BaseConfig;
-    
-    // 服务标识
-    private $serverMark = 'Vcode';
-    
-    public function __construct(array $config = [], array $prepends = [], string $id = null)
-    {
-        parent::__construct($this->initConfig($config, $this->serverMark), $this->serverMark, true);
+  use Response;
+  use BaseConfig;
+  
+  // 服务标识
+  private $serverMark = 'Vcode';
+  
+  public function __construct(array $config = [], array $prepends = [], string $id = null)
+  {
+    parent::__construct($this->initConfig($config, $this->serverMark), $this->serverMark, true);
+  }
+  
+  public function send($params = [])
+  {
+    if (strlen(trim($params['receive'])) == 11) {
+      return $this->httpPostJsonV1(
+        array_merge([
+          'action' => 'send',
+        ], $params)
+      );
+    } else {
+      return $this->httpPostJsonV1(
+        array_merge([
+          'action'     => 'send',
+          'config_key' => $this->config['config_key'],
+        ], $params)
+      );
     }
-    
-    public function send($params = [])
-    {
-        return $this->httpPostJsonV1(
-            array_merge([
-                'action' => 'send',
-                'config_key' => $this->config['config_key'],
-            ], $params)
-        );
+  }
+  
+  public function check($params = [])
+  {
+    if (strlen(trim($params['params'])) == 11) {
+      return $this->httpPostJsonV1(
+        array_merge([
+          'action' => 'check',
+        ], $params)
+      );
+    } else {
+      return $this->httpPostJsonV1(
+        array_merge([
+          'action' => 'check',
+          'config_key' => $this->config['config_key'],
+        ], $params)
+      );
     }
-    
-    public function check($params = [])
-    {
-        return $this->httpPostJsonV2(
-            array_merge([
-                'action' => 'check',
-            ], $params)
-        );
-    }
+  }
 }
