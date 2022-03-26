c5c4a4edf1f469b2a4809bda9ee27291ae3d1f74
guozi <33157@163.com>
2022-03-26 20:28:05 +0800

    1.3.16

--- src/OfficialAccount/Other/ServiceProvider.php
+++ src/OfficialAccount/Other/ServiceProvider.php
@@ -7,29 +7,51 @@ use JuheApi\BasicService\RequestContainer;
 
 class ServiceProvider extends RequestContainer
 {
-    public function __construct($config = null)
-    {
-        parent::__construct($config);
-    }
-    
-    public function getToken()
-    {
-        return $this->httpPostJsonV2(
-            [
-                'action' => 'getToken',
-            ]
-        );
-    }
-    
-    public function getEnv($path){
-        return Env::get($path);
-    }
-    
-    public function getEnvmpConfigKey($name){
-        return $this->getEnv('mpConfigKey.' . $name);
-    }
-    
-    public function getEnvminiConfigKey($name){
-        return $this->getEnv('miniConfigKey.' . $name);
-    }
+  public function __construct($config = null)
+  {
+    parent::__construct($config);
+  }
+  
+  public function getToken()
+  {
+    return $this->httpPostJsonV2(
+      [
+        'action' => 'getToken',
+      ]
+    );
+  }
+  
+  public function getEnv($path)
+  {
+    return Env::get($path);
+  }
+  
+  public function getEnvmpConfigKey($name)
+  {
+    return $this->getEnv('mpConfigKey.' . $name);
+  }
+  
+  public function getEnvminiConfigKey($name)
+  {
+    return $this->getEnv('miniConfigKey.' . $name);
+  }
+  
+  public function getJsTicket()
+  {
+    return $this->httpPostJsonV2(
+      [
+        'action' => 'getJsTicket',
+      ], [], false
+    );
+  }
+  
+  public function getSignPackage($url)
+  {
+    return $this->httpPostJsonV2(
+      [
+        'action' => 'getSignPackage',
+        'url'    => $url,
+      ], [], false
+    );
+  }
 }
